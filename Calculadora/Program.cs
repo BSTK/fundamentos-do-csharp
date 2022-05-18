ca4aca7c6b632ea2e74c7dcc7d21ad058250607d
Bruno Luz <brunoluz@sisconsultoria.com.br>
2022-05-18 16:04:06 -0300

    main - Criando uma calculadora CLI

---
+++ Calculadora/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Calculadora
+{
+    public class Program 
+    {
+        public static void Main()
+        {
+
+            Console.Clear();
+            Terminal.TextoCentralizadoNovaLinha("-----------------");
+            Terminal.TextoCentralizadoNovaLinha("** Calculadora **");
+            Terminal.TextoCentralizadoNovaLinha("-----------------");
+
+            Terminal.TextoCentralizado("Primeiro número: ");
+            float primeiroNumero = Convert.ToSingle(Console.ReadLine());
+
+            Terminal.TextoCentralizado("Segundo número: ");
+            float segundoNumero = Convert.ToSingle(Console.ReadLine());
+
+            float resultado = primeiroNumero + segundoNumero;
+
+            Console.WriteLine();
+            Terminal.TextoCentralizadoNovaLinha(
+                $"Soma de { primeiroNumero } + { segundoNumero } = { resultado }"
+            );
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Calculadora/Terminal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Calculadora
+{
+    public class Terminal
+    {
+        private static readonly int WINDOW_WIDTH_TEXTO_CENTRALIZADO = Console.WindowWidth / 2;
+
+        public static void TextoCentralizado(string texto)
+        {
+            var margemEsquerda = MargemEsquerda(texto);
+            Console.Write(texto.PadLeft(margemEsquerda));
+        }
+
+        public static void TextoCentralizadoNovaLinha(string texto)
+        {
+            var margemEsquerda = MargemEsquerda(texto);
+            Console.WriteLine(texto.PadLeft(margemEsquerda));
+        }
+
+        private static int MargemEsquerda(string texto)
+        {
+            if (texto == null)
+            {
+                throw new ArgumentNullException(texto, "Texto não pode ser nulo!");
+            }
+
+            return WINDOW_WIDTH_TEXTO_CENTRALIZADO + (texto.Length / 2);
+        }
+    }
+}
